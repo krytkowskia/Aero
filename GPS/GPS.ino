29c33b844e781dfd27ce92f01306dea790440a33
krytkowskia <krytkowskia@msoe.edu>
2014-10-27 18:05:44 -0500

    Converted coordinates into tokens. Need to convert tokens to degrees, mins, seconds.

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -23,6 +23,20 @@ void loop(){
     
     /* Check for the end of the coordinate */
     if(character == '\n'){
+      /* The beginning of the GPS coordinates starts with $GPRMC */
+      if(coordinates.startsWith("$GPRMC")){
+        char coords[128];
+        char *coordsPtr = coords;
+        char *tokens; 
+
+        /* Place coordinates into char array point to separate into tokens */
+        coordinates.toCharArray(coords, coordinate.length());
+        /* Break NMEA into coordinate tokens */
+        tokens = strtok_r(coordPtr, ",", &coordsPtr);
+        while(tokens != NULL){
+          /* Convert NMEA into Degrees, mins, seconds */
+        }
+      }
       /* Check for the beginning of a coordinates */
       /* Calculate the coordinates into degrees, mins, seconds */
     } else{
