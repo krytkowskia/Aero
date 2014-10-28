0f9fd2f0f1fa65ba272d1532f2ef8998d54c3a2c
krytkowskia <krytkowskia@msoe.edu>
2014-10-28 07:29:14 -0500

    Convert the temperature to fahrenheit and converted the altitude to feet.

--- Altimeter/Altimeter.ino
+++ Altimeter/Altimeter.ino
@@ -2,6 +2,9 @@
 *  Aero Team
 *  Altimeter, Barometric Pressure, Temperature
 *  10-27-2014
+*
+*  Pull up resistors are located on pins 20 and 21
+*
 *************************************/
 
 #include <Wire.h>
@@ -19,16 +22,21 @@ void loop(void){
   sensors_event_t event;
   BMP.getEvent(&event);
   
+  /* Convert altitude, meters to feet */
+  float feet = BMP.pressureToAltitude(SENSORS_PRESSURE_SEALEVELHPA, event.pressure) * 3.28084;
   Serial.print("Altitude:    ");
-  Serial.print(BMP.pressureToAltitude(SENSORS_PRESSURE_SEALEVELHPA, event.pressure));
-  Serial.println(" m");
+  Serial.print(feet);
+  Serial.println(" ft");
   
   float temperature;
   BMP.getTemperature(&temperature);
+  /* Convert temperature, celsius to fahrenheit */
+  float fahrenheit = temperature * 33.8;
   Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print(" C");
+  Serial.print(fahrenheit);
+  Serial.print(" F");
   
+  /* Standard Atmospheric Pressure = 1,013.25 hPA */
   Serial.print("Pressure:    ");
   Serial.print(event.pressure);
   Serial.println(" hPa\n");
